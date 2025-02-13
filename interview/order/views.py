ad2b679a3e9a981b8a9993ceebfd355f2ea883fa
Katherine Foley <katherine.foley@willolabs.com>
2025-02-13 09:48:34 -0500

    challenge 6

--- interview/order/views.py
+++ interview/order/views.py
@@ -13,3 +13,16 @@ class OrderListCreateView(generics.ListCreateAPIView):
 class OrderTagListCreateView(generics.ListCreateAPIView):
     queryset = OrderTag.objects.all()
     serializer_class = OrderTagSerializer
+
+
+class OrderTagListView(generics.ListCreateAPIView):
+    queryset = OrderTag.objects.all()
+    serializer_class = OrderTagSerializer
+
+    def get(self, request: Request, *args, **kwargs) -> Response:
+        order_id = kwargs['order_id']
+        tag_id_list = Order.objects.get(id=order_id).select_related('tags').values_list('tags__id')
+        order_tags = self.queryset.filter(id__in=tag_id_list)
+        serializer = self.serializer_class(order_tags)
+
+        return Response(serializer.data, status=200)
